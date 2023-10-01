438ec6e51a6ba8ec9f3d5b47c4682524751c12fc
kalio007 <kalioprince1@gmail.com>
2023-10-01 01:03:10 +0100

    feature: added the get request

--- controller/upload.js
+++ controller/upload.js
@@ -4,12 +4,17 @@ const cloudinary = require('../utils/cloudinary');
 const accept = async (req, res) => {
     try {
         // Upload the image
-        const result = await cloudinary.uploader.upload(req.file.path);
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            resource_type: "auto",
+            use_filename: true,
+            raw_convert: "google_speech"
+        });
         // console.log(result);
         return res.status(200).json({
             success: true,
             message: "Uploaded!",
-            data: result.url
+            public_id: result.public_id,
+            video_url: result.url
         });
     } catch (error) {
         // console.error(error);
@@ -19,24 +24,29 @@ const accept = async (req, res) => {
         })
     }
 }
-// const getAssetInfo = async (req, res) => {
-//     const public_id = req.params.public_id
-//     try {
-//         // Get details about the asset
-//         const result = await cloudinary.api.resource(public_id);
-//         // console.log(result);
-//         return res.status(200).json({
-//             message: "found video",
-//             statusCode: 200,
-//             data: result.url,
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({
-//             message: "internal error",
-//             data: null
-//         })
-//     }
-// };
+const getVideo = async (req, res) => {
+    const public_id = req.params.public_id
+    try {
+        // Get details about the asset
+        const result = await cloudinary.video(public_id, {
+            transformation: [
+                { overlay: { resource_type: "subtitles", public_id: `${public_id}.transcript` } },
+                { flags: "layer_apply" }
+            ]
+        });
+        // console.log(result);
+        return res.status(200).json({
+            message: "found video",
+            statusCode: 200,
+            data: result,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "internal error",
+            data: null
+        })
+    }
+};
 
-module.exports = { accept }
+module.exports = { accept, getVideo }
--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const { accept } = require('../controller/upload')
+const { accept, getVideo } = require('../controller/upload')
 const upload = require('../middleware/multer')
 
 
-router.post('/upload', upload.single('file'), accept)
-// router.get('/:public_id', getAssetInfo)
+router.post('/upload', upload.single('video'), accept)
+router.get('/:public_id', getVideo)
 
 module.exports = router;
